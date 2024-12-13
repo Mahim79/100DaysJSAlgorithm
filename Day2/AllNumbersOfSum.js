221eded5f6b8b740823b9ca7bcefc8282e282903
Mahim Dewan <mahimdewan79@gmail.com>
2024-12-13 23:13:56 +0600

    Day2 sum of all numbers

---
+++ Day2/AllNumbersOfSum.js
@@ -0,0 +1,10 @@
+
+function allNumbersOfSum(...params){
+    let sum = 0;
+    params.forEach((element)=>{
+        sum += element
+    })
+    return sum
+}
+
+console.log(allNumbersOfSum(5,35,10,50));
