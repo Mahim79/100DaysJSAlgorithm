657a0e84d5f8d0802b69b8fd8face101019c9fa3
Mahim Dewan <mahimdewan79@gmail.com>
2024-12-27 23:50:40 +0600

    Most popular and easy FizzBuzz Problem in JS

---
+++ Day10/fizzbuzz.js
@@ -0,0 +1,21 @@
+
+function fizzbuzz(start,end){
+    for(let i=start; i <= end; i++){
+        if(i % 3 === 0 && i % 5 === 0){
+            console.log("FizzBuzz");
+            
+        }else if(i % 3 === 0){
+            console.log("Fizz");
+            
+        }else if(i % 5 === 0){
+            console.log("Buzz");
+            
+        }else{
+            console.log(i);
+            
+        }
+    }
+    
+}
+
+fizzbuzz(1,20)
