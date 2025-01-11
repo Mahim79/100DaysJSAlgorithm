5143deee4df71c686ceceb8d870e9ff9ec53a1a2
Mahim Dewan <mahimdewan79@gmail.com>
2025-01-11 22:27:06 +0600

    Sort an array of integers in an ascending order

---
+++ Day13/sortInteger.js
@@ -0,0 +1,13 @@
+const integers = [2,5,12,18,5,7,3,22]
+
+
+const sortIntegers = (integers)=>{
+    return integers.sort((a,b)=> a-b) // There are used compare function to ascending sort
+}
+
+console.log(sortIntegers(integers));
+
+
+
+
+
